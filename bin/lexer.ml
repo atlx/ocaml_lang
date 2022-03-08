4a6fef96ef2b5df24f5b729109884e63d39c27a6
atlx <41349879+atlx@users.noreply.github.com>
2022-03-08 03:41:56 -0500

    Add various other tokens

--- bin/lexer.ml
+++ bin/lexer.ml
@@ -1,9 +1,17 @@
 module Lexer = struct
-  type token = Illegal | Identifier of string | Plus | EOF
+  type token =
+    | Illegal
+    | Identifier of string
+    | Plus
+    | Minus
+    | Whitespace
+    | EOF
 
   (** Determine whether a character is within the (inclusive) range of a-z or A-Z. *)
-  let is_identifier_char ch =
+  let is_identifier ch =
     ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z') || ch == '_'
+
+  let is_whitespace = function ' ' | '\t' | '\n' | '\r' -> true | _ -> false
 
   (* let is_digit ch = '0' <= ch && ch <= '9' *)
 
@@ -21,7 +29,7 @@ module Lexer = struct
       | [] -> (string_of_char_list acc, [])
       | head :: tail -> (
           match head with
-          | _ when is_identifier_char head -> aux (head :: acc) tail
+          | _ when is_identifier head -> aux (head :: acc) tail
           | _ -> (string_of_char_list acc, tail))
     in
     aux [] char_list
@@ -35,9 +43,11 @@ module Lexer = struct
     | head :: tail -> (
         match head with
         | '+' -> (Plus, tail)
-        | _ when is_identifier_char head ->
-            let result = lex_identifier char_list in
-            (Identifier (fst result), snd result)
+        | '-' -> (Minus, tail)
+        | _ when is_whitespace head -> (Whitespace, tail)
+        | _ when is_identifier head ->
+            let identifier = lex_identifier char_list in
+            (Identifier (fst identifier), snd identifier)
         | _ -> (Illegal, tail))
 
   (** Collect all possible tokens from a string. *)
@@ -52,17 +62,20 @@ module Lexer = struct
             aux (snd lex_result) (fst lex_result) (tok :: acc)
     in
     let char_list = explode str in
-    aux char_list (lex_token char_list |> fst) [] |> List.rev
+    let prime_lex_result = lex_token char_list in
+    aux (snd prime_lex_result) (fst prime_lex_result) [] |> List.rev
 
   let string_of_token = function
     | Illegal -> "unknown"
     | Identifier value -> "identifier(" ^ value ^ ")"
     | Plus -> "plus"
+    | Minus -> "minus"
+    | Whitespace -> "whitespace"
     | EOF -> "eof"
 end
 
 let () =
-  Lexer.lex "hello world +++"
+  Lexer.lex "hello world   --- "
   |> List.map Lexer.string_of_token
   |> String.concat ", " |> print_endline
 (*[ 'a'; 'b'; 'c' ] |> Lexer.string_of_char_list |> print_endline*)
