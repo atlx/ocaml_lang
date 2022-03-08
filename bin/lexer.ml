3cca41ce1c624b090c85e3e301c824bde9839a38
atlx <41349879+atlx@users.noreply.github.com>
2022-03-08 04:11:08 -0500

    Fix identifier lexing bug

--- bin/lexer.ml
+++ bin/lexer.ml
@@ -24,18 +24,19 @@ module Lexer = struct
     aux "" (List.rev char_list)
 
   (** Create an [Identifier] token from the given character list. *)
-  let lex_identifier char_list =
-    let rec aux acc = function
+  let lex_identifier chars =
+    let rec aux acc char_list =
+      match char_list with
       | [] -> (string_of_char_list acc, [])
       | head :: tail -> (
           match head with
           | _ when is_identifier head -> aux (head :: acc) tail
-          | _ -> (string_of_char_list acc, tail))
+          | _ -> (string_of_char_list acc, char_list))
     in
-    aux [] char_list
+    aux [] chars
 
-  let lex_token char_list =
-    match char_list with
+  let lex_token chars =
+    match chars with
     | [] -> (EOF, [])
     | head :: tail -> (
         match head with
@@ -43,24 +44,21 @@ module Lexer = struct
         | '-' -> (Minus, tail)
         | _ when is_whitespace head -> (Whitespace, tail)
         | _ when is_identifier head ->
-            let identifier = lex_identifier char_list in
+            let identifier = lex_identifier chars in
             (Identifier (fst identifier), snd identifier)
         | _ -> (Illegal, tail))
 
   (** Collect all possible tokens from a string. *)
   let lex str =
-    let rec aux char_list acc =
-      match char_list with
+    let rec aux acc = function
       | [] -> acc
-      | _ :: _ ->
-          if char_list = [] then acc
-          else
-            let lex_result = lex_token char_list in
-            aux (snd lex_result) (fst lex_result :: acc)
+      | chars ->
+          let lex_result = lex_token chars in
+          aux (fst lex_result :: acc) (snd lex_result)
     in
     (* Deconstruct the string into a list of characters. *)
-    let char_list = List.init (String.length str) (String.get str) in
-    aux char_list [] |> List.rev
+    let chars = List.init (String.length str) (String.get str) in
+    aux [] chars |> List.rev
 
   let string_of_token = function
     | Illegal -> "unknown"
@@ -72,7 +70,7 @@ module Lexer = struct
 end
 
 let () =
-  Lexer.lex "h world how are you  --- "
+  Lexer.lex "h how are you"
   |> List.map Lexer.string_of_token
   |> String.concat ", " |> print_endline
 (*[ 'a'; 'b'; 'c' ] |> Lexer.string_of_char_list |> print_endline*)
