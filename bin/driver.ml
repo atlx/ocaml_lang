21cc01a68e4368f48029efce3221d8fdec1aaa24
atlx <41349879+atlx@users.noreply.github.com>
2022-04-17 02:40:08 -0400

    Initial lowering foundations

--- bin/driver.ml
+++ bin/driver.ml
@@ -1,12 +1,8 @@
-open Ocaml_lang.Lexer
-open Ocaml_lang.Parser
+(* open Ocaml_lang.Lexer open Ocaml_lang.Parser *)
+open Ocaml_lang.Lowering
 
-let () =
-  let tokens =
-    read_line () |> Lexer.lex
-    |> List.filter (fun tok -> tok != Lexer.Whitespace)
-  in
-  match Parser.parse tokens with
-  | Ok ast ->
-    (List.map Parser.string_of_ast) ast |> String.concat ", " |> print_endline
-  | Error msg -> print_endline ("parse error: " ^ msg)
+(* let () = let tokens = read_line () |> Lexer.lex |> List.filter (fun tok ->
+   tok != Lexer.Whitespace) in match Parser.parse tokens with | Ok ast ->
+   (List.map Parser.string_of_ast) ast |> String.concat ", " |> print_endline |
+   Error msg -> print_endline ("parse error: " ^ msg) *)
+let () = Lowering.run ()
--- lib/lowering.ml
+++ lib/lowering.ml
@@ -1,3 +1,48 @@
-(* open Llvm
+open Parser
 
-   let a = Type.i8 let () = print_endline "hello from llvm lowering!" *)
+type 'a state = 'a * Llvm.llvalue * Llvm.llbasicblock
+
+let modify_state (state : 'a state) f : 'a state =
+  let value, fn, block = state in
+  f (value, fn, block)
+
+let context = Llvm.global_context ()
+let module_ = Llvm.create_module context "program"
+let main_type = Llvm.function_type (Llvm.i32_type context) [||]
+let main = Llvm.define_function "main" main_type module_
+let builder = Llvm.builder context
+
+let visit_block_state block (visit : unit state -> Parser.ast -> unit)
+    (state : unit state) : unit state =
+  let next_state =
+    modify_state state (fun (value, fn, _) ->
+        (value, fn, Llvm.append_block context "block" main))
+  in
+  match block with
+  | Parser.Block statements ->
+    List.iter (fun el -> visit next_state el) statements;
+    next_state
+  | _ ->
+    ();
+    next_state
+(* let (statements) = block in List.iter statements visit in modify_state state
+   (fun (value, fn, _) -> (value, fn, new_llvm_block)) *)
+
+let visit_block block visit =
+  (* Llvm.append_block context "block" main; *)
+  match block with
+  | Parser.Block statements -> begin
+    match statements with [] -> () | lst -> List.iter visit lst
+  end
+  | _ -> ()
+
+module Lowering = struct
+  (* let visit_return_stmt visit return_stmt *)
+
+  let rec visit (state : unit state) = function
+    (* | Parser.Block _ as block -> visit_block block visit *)
+    (* | Parser.ReturnStmt _ as return_stmt -> visit *)
+    | _ -> ()
+
+  let run () = print_endline (Llvm.string_of_llvalue main)
+end
